f492a876f3e2e0eb4125e5a61f32a439a1429d05
Ricky Anshuman Dash <108886729+rdashcr7@users.noreply.github.com>
2025-10-06 07:23:34 -0400

    run_MMPBSA.sh

--- run_MMPBSA.sh
+++ run_MMPBSA.sh
@@ -7,81 +7,246 @@ source activate ~/MMPBSA_env/  # enter path to directory containing this script,
 
 # === Clean workspace ===
 rm -f *.prmtop *.inpcrd *.mdcrd *.log *.dat *.out *.pdb *.dcd *.txt *.csv tleap_*.in tleap_*.log *.json
+rm reference.frc
+rm -f _MMPBSA_*
+rm -f mmpbsa.in
+rm -f mmpbsa1.in
+rm -f mmpbsa2.in
+rm -f mmpbsa3.in
+rm -f mmpbsa4.in
+rm -f mmpbsa5.in
 
 python3 ligand_receptor_binding.py 
 
-# Path to forcefield files, this should be in the directory of pdb2amber
-FF_PROTEIN="./pdb2amber/data/protein.ff14SB.xml"
-FF_WATER="./pdb2amber/data/wat_opc3.xml"
+python3 fix_traj.py
 
-pdb4amber -i NVT_complex_dry.pdb -o NVT_complex_dry1.pdb --nohyd
+ante-MMPBSA.py -p complex.prmtop -c complex.inpcrd -r receptor.prmtop -l ligand.prmtop -m ":1-98"
 
-cat > tleap_complex.in << EOF
-source leaprc.protein.ff14SB
-mol = loadpdb NVT_complex_dry1.pdb
-saveamberparm mol complex.prmtop complex.inpcrd
-quit
-EOF
-
-tleap -f tleap_complex.in > tleap_complex.out 2>&1
-
-
-ante-MMPBSA.py -p complex.prmtop -c complex.inpcrd -r receptor.prmtop -l ligand.prmtop -m ":1-98"  # in m, insert the residue numbers for the receptor as this will help ante-MMPBSA to mask the receptor and ligand separately for the prmtop files of ligand and receptor
-
-cat > input_complex.json <<EOF
-{
-    "fname_pdb": "NVT_complex.pdb",
-    "fname_prmtop": "complex_solvated.prmtop",
-    "fname_inpcrd": "complex_solvated.inpcrd",
-    "fname_ff": [
-        "${FF_PROTEIN}",
-        "${FF_WATER}"
-    ]
-}
-EOF
-python3 pdb2amber/pdb2amber.py -i input_complex.json
-
-cat > convert_traj.in <<EOF
-parm complex_solvated.prmtop
-trajin complex_trajectory.dcd
-trajout trajectory.mdcrd
-EOF
-
-cpptraj -i convert_traj.in > cpptraj.log 2>&1
+mdconvert -o trajectory_cleaned.nc -t complex.prmtop trajectory_dry.dcd
 
 cat > mmpbsa.in <<EOF
 &general
    startframe=1,
-   endframe=25000000,
-   interval=1000,
-   verbose=1,
+   endframe=25001,
+   interval=250,
    keep_files=0,
-   strip_mask=":WAT,HOH,Na+,Cl-,Na,Cl",  
-   use_sander=0,
 /
 
 &pb
-   istrng=0.100,          
-   exdi=80.0,             
-   indi=1.0,              
-   inp=2,                
-   radiopt=1,             
-   cavity_surften=0.0378,
-   cavity_offset=-0.5692,
+   istrng=0.150,
+   exdi=80.0,
+   indi=4.0,
+   inp=2,
+   radiopt=0,
+   cavity_surften=0.00542,
+   cavity_offset=0.92,
    scale=2.0,
    prbrad=1.4,
    fillratio=4.0,
    linit=1000,
 /
-EOF
 
+&gb
+   igb=5,
+   saltcon=0.150,
+/
+EOF
 
 MMPBSA.py -O \
   -i mmpbsa.in \
   -o binding_energy.dat \
-  -sp complex_solvated.prmtop \
   -cp complex.prmtop \
   -rp receptor.prmtop \
   -lp ligand.prmtop \
-  -y trajectory.mdcrd
+  -y trajectory_cleaned.nc
 
+# first 5000 frames
+cat > mmpbsa1.in <<EOF
+&general
+   startframe=1,
+   endframe=5001,
+   interval=50,
+   keep_files=0,
+   entropy=1,
+/
+
+&pb
+   istrng=0.150,
+   exdi=80.0,
+   indi=4.0,
+   inp=2,
+   radiopt=0,
+   cavity_surften=0.00542,
+   cavity_offset=-1.008,
+   scale=2.0,
+   prbrad=1.4,
+   fillratio=4.0,
+   linit=1000,
+/
+
+&gb
+   igb=5,
+   saltcon=0.150,
+/
+EOF
+
+MMPBSA.py -O \
+  -i mmpbsa1.in \
+  -o binding_energy1.dat \
+  -cp complex.prmtop \
+  -rp receptor.prmtop \
+  -lp ligand.prmtop \
+  -y trajectory_cleaned.nc
+
+# 5000-10000 frames
+cat > mmpbsa2.in <<EOF
+&general
+   startframe=5001,
+   endframe=10001,
+   interval=50,
+   keep_files=0,
+   entropy=1,
+/
+
+&pb
+   istrng=0.150,
+   exdi=80.0,
+   indi=4.0,
+   inp=2,
+   radiopt=0,
+   cavity_surften=0.00542,
+   cavity_offset=-1.008,
+   scale=2.0,
+   prbrad=1.4,
+   fillratio=4.0,
+   linit=1000,
+/
+
+&gb
+   igb=5,
+   saltcon=0.150,
+/
+EOF
+
+MMPBSA.py -O \
+  -i mmpbsa2.in \
+  -o binding_energy2.dat \
+  -cp complex.prmtop \
+  -rp receptor.prmtop \
+  -lp ligand.prmtop \
+  -y trajectory_cleaned.nc
+
+# 10000 - 15000 frames
+cat > mmpbsa3.in <<EOF
+&general
+   startframe=10001,
+   endframe=15001,
+   interval=50,
+   keep_files=0,
+   entropy=1,
+/
+
+&pb
+   istrng=0.150,
+   exdi=80.0,
+   indi=4.0,
+   inp=2,
+   radiopt=0,
+   cavity_surften=0.00542,
+   cavity_offset=-1.008,
+   scale=2.0,
+   prbrad=1.4,
+   fillratio=4.0,
+   linit=1000,
+/
+
+&gb
+   igb=5,
+   saltcon=0.150,
+/
+EOF
+
+MMPBSA.py -O \
+  -i mmpbsa3.in \
+  -o binding_energy3.dat \
+  -cp complex.prmtop \
+  -rp receptor.prmtop \
+  -lp ligand.prmtop \
+  -y trajectory_cleaned.nc
+
+# 15000 - 20000 frames
+cat > mmpbsa4.in <<EOF
+&general
+   startframe=15001,
+   endframe=20001,
+   interval=50,
+   keep_files=0,
+   entropy=1,
+/
+
+&pb
+   istrng=0.150,
+   exdi=80.0,
+   indi=4.0,
+   inp=2,
+   radiopt=0,
+   cavity_surften=0.00542,
+   cavity_offset=-1.008,
+   scale=2.0,
+   prbrad=1.4,
+   fillratio=4.0,
+   linit=1000,
+/
+
+&gb
+   igb=5,
+   saltcon=0.150,
+/
+EOF
+
+MMPBSA.py -O \
+  -i mmpbsa4.in \
+  -o binding_energy4.dat \
+  -cp complex.prmtop \
+  -rp receptor.prmtop \
+  -lp ligand.prmtop \
+  -y trajectory_cleaned.nc
+
+
+# 20000 - 25000 frames
+cat > mmpbsa5.in <<EOF
+&general
+   startframe=20001,
+   endframe=25001,
+   interval=50,
+   keep_files=0,
+   entropy=1,
+/
+
+&pb
+   istrng=0.150,
+   exdi=80.0,
+   indi=4.0,
+   inp=2,
+   radiopt=0,
+   cavity_surften=0.00542,
+   cavity_offset=-1.008,
+   scale=2.0,
+   prbrad=1.4,
+   fillratio=4.0,
+   linit=1000,
+/
+
+&gb
+   igb=5,
+   saltcon=0.150,
+/
+EOF
+
+MMPBSA.py -O \
+  -i mmpbsa5.in \
+  -o binding_energy5.dat \
+  -cp complex.prmtop \
+  -rp receptor.prmtop \
+  -lp ligand.prmtop \
+  -y trajectory_cleaned.nc
